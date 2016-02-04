e3f17c0f51309da6fc28b8b7446c716646bc7d88
tomkim75 <tomkim75@gmail.com>
2016-02-04 00:39:37 -0800

    Added const iterator to map.

--- containers/TMap.h
+++ containers/TMap.h
@@ -7,6 +7,7 @@ class TMapPair
 {
     template <typename K, typename V> friend class TMap;
     template <typename K, typename V> friend class TMapItr;
+    template <typename K, typename V> friend class TMapConstItr;
 
 public:
 
@@ -29,6 +30,7 @@ class TMapItr
     typedef TMapPair<K, V> Pair;
     typedef TRbTreeItr<Pair> BaseItr;
     template <typename K, typename V> friend class TMap;
+    template <typename K, typename V> friend class TMapConstItr;
 
 public:
 
@@ -36,6 +38,8 @@ public:
 
     bool operator==(const TMapItr& other) const { return m_baseItr.operator==(other.m_baseItr); }
     bool operator!=(const TMapItr& other) const { return m_baseItr.operator!=(other.m_baseItr); }
+    bool operator==(const TMapConstItr<K, V>& other) const { return m_baseItr.operator==(other.m_baseItr); }
+    bool operator!=(const TMapConstItr<K, V>& other) const { return m_baseItr.operator!=(other.m_baseItr); }
     TMapItr& operator++(void) { ++m_baseItr; return *this; }
     TMapItr& operator--(void) { --m_baseItr; return *this; }
     V& operator*(void) { Pair& pair = *m_baseItr; return pair.m_value; }
@@ -48,12 +52,40 @@ private:
 };
 
 template <typename K, typename V>
+class TMapConstItr
+{
+    typedef TMapPair<K, V> Pair;
+    typedef TRbTreeConstItr<Pair> BaseItr;
+    template <typename K, typename V> friend class TMap;
+    template <typename K, typename V> friend class TMapItr;
+
+public:
+
+    TMapConstItr(const TMapConstItr& other) : m_baseItr(other.m_baseItr) { }
+    TMapConstItr(const TMapItr<K, V>& other) : m_baseItr(other.m_baseItr) { }
+
+    bool operator==(const TMapConstItr& other) const { return m_baseItr.operator==(other.m_baseItr); }
+    bool operator!=(const TMapConstItr& other) const { return m_baseItr.operator!=(other.m_baseItr); }
+    bool operator==(const TMapItr<K, V>& other) const { return m_baseItr.operator==(other.m_baseItr); }
+    bool operator!=(const TMapItr<K, V>& other) const { return m_baseItr.operator!=(other.m_baseItr); }
+    TMapConstItr& operator++(void) { ++m_baseItr; return *this; }
+    TMapConstItr& operator--(void) { --m_baseItr; return *this; }
+    const V& operator*(void) const { const Pair& pair = *m_baseItr; return pair.m_value; }
+
+private:
+
+    BaseItr m_baseItr;
+
+    TMapConstItr(const BaseItr& baseItr) : m_baseItr(baseItr) { }
+};
+
+template <typename K, typename V>
 class TMap : private TRbTree<TMapPair<K, V> >
 {
 public:
 
     typedef TMapItr<K, V> Iterator;
-    //typedef TRbTreeConstItr<K, V> ConstIterator;
+    typedef TMapConstItr<K, V> ConstIterator;
 
     void insert(const K& key, const V& value) { TRbTree::insert(TMapPair<K, V>(key, value)); }
     void erase(const K& key) { TRbTree::erase(TMapPair(key)); }
