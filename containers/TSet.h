40d7d5bb60cdb0ca037666bb74097640dd03dd5e
tomkim75 <tomkim75@gmail.com>
2016-02-11 22:46:39 -0800

    Removed example from TSet.h because it was not very good.  Added copyright to TList.h.

--- containers/TSet.h
+++ containers/TSet.h
@@ -2,24 +2,6 @@
 Copyright 2016 Tom Kim
 Implementation of a set container backed by a red-black tree with an STL-like
 interface.
-
-Example:
-
-    typedef TSet<std::string> Car;
-    Car car;
-    car.insert("volkswagen");
-    car.insert("jetta");
-    car.insert("1995");
-    car.insert("black");
-
-    Car::iterator itr = car.find("black");
-    car.erase(itr);                         // erase "black"
-    car.insert("red");                      // insert "red"
-
-    for (Car::const_iterator itr = car.begin(); itr != car.end(); ++itr)
-    {
-        std::cout << *itr << std::endl;
-    }
 */
 #pragma once
 
